dfaddfb088d00b82d0bdc850ed496f036a6473f7
Idris8303 <bctxtoast@gmail.com>
2017-08-17 10:16:00 -0500

    pushing more stuff

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const app = express();
 app.use(express.static('public'));
 
 
+console.log('================');
+console.log(process.env);
+
 
 app.listen(7990, ()=>{
   console.log('listening on port 7990');
